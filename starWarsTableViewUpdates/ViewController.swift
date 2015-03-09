b8de413effacb0a5898946d42303f470dd38fbcd
Christina Moulton <christina@teakmobile.com>
2015-03-08 20:23:10 -0400

    Adding & removing rows though 3 clicks on the + button

--- starWarsTableViewUpdates/ViewController.swift
+++ starWarsTableViewUpdates/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
   
   @IBOutlet weak var tableView: UITableView?
   var movies:Array<String>?
+  var tapCount = 0
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -26,11 +27,44 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
   }
   
   func addPrequels(sender: AnyObject) {
-    movies?.insert("Episode 1", atIndex: 0)
-    movies?.insert("Episode 2", atIndex: 1)
-    movies?.insert("Episode 3", atIndex: 2)
-
-    self.tableView?.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0), NSIndexPath(forRow: 1, inSection: 0), NSIndexPath(forRow: 2, inSection: 0)], withRowAnimation: .Automatic)
+    tapCount++
+    if (tapCount == 1)
+    {
+      // Add the prequels if we're not showing them yet
+      movies?.insert("The Phantom Menace", atIndex: 0)
+      movies?.insert("The Attack of the Clones", atIndex: 1)
+      movies?.insert("Revenge of the Sith", atIndex: 2)
+      
+      // use a single call to update the tableview with 3 indexpaths
+      self.tableView?.insertRowsAtIndexPaths(
+        [NSIndexPath(forRow: 0, inSection: 0),
+          NSIndexPath(forRow: 1, inSection: 0),
+          NSIndexPath(forRow: 2, inSection: 0)],
+        withRowAnimation: .Automatic)
+    }
+    else if (tapCount == 2)
+    {
+      // Add the sequels, even though they don't exist yet
+      // use one call to delete and one call to add rows to the tableview
+      // so we need to wrap them in beginUpdates()/endUpdates()
+      // add the sequels
+      if let count = movies?.count
+      {
+        movies?.append("The Force Awakens")
+        movies?.append("Episode VIII")
+        movies?.append("Episode IX")
+        self.tableView?.insertRowsAtIndexPaths([NSIndexPath(forRow: count, inSection: 0),
+          NSIndexPath(forRow: count+1, inSection: 0),
+          NSIndexPath(forRow: count+2, inSection: 0)], withRowAnimation: .Automatic)
+      }
+    }
+    else if (tapCount == 3)
+    {
+      movies?.removeAtIndex(0)
+      movies?.removeAtIndex(0)
+      movies?.removeAtIndex(0)
+      self.tableView?.deleteRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0), NSIndexPath(forRow: 1, inSection: 0), NSIndexPath(forRow: 2, inSection: 0)], withRowAnimation: .Automatic)
+    }
   }
   
   // MARK: - TableView
